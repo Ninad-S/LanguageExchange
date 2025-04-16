afc907e4b5cf1d0182357d9158796573a7b45d18
magbuv <mbuvan14@gmail.com>
2025-04-16 15:14:42 -0500

    added changes

--- src/pages/LanguageQuiz.jsx
+++ src/pages/LanguageQuiz.jsx
@@ -2,10 +2,13 @@
 //Language Quiz implementation by Maggie Buvanendiran
 import React, { useEffect, useState } from 'react';
 import {
+  getFirestore,
   collection,
+  getDocs,
   onSnapshot,
   query,
   serverTimestamp,
+  QuerySnapshot,
 } from 'firebase/firestore';
 import { db } from '../firebase';
 
@@ -17,34 +20,74 @@ const LanguageQuiz = () => {
   const [selectedAnswer, setSelectedAnswer] = useState('')
   //assigns variables for the results, including the score, correct and incorrect answers set to the default count of 0
   const [result, setResult] = useState({
-    //finalScore: 0,
-    //correctAnswers: 0,
-    //incorrectAnswers: 0,
+    finalScore: 0,
+    correctAnswers: 0,
+    incorrectAnswers: 0,
   })
   const [showFinalResult, setShowFinalResult] = useState(false)
   const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null)
+  const [color, setColor] = useState("grey");
 
   //uses the getQuestions function to receive questions from the database to be used in the quiz
   //assigns a question in random order into the quiz along with the answers.
+  /*const quiz = {
+    questions: [
+      {
+        question: 'pick 2?',
+        answers: ['1', '2', '3', '4'],
+        answerKey: '2',
+        questionType: 'MCQ'
+      },
+      {
+        question: 'say hello!',
+        answers: ['hello'],
+        answerKey: 'hello',
+        questionType: 'short answer'
+      }
+    ],
+  }*/
+  
+  const [questions, setQuestions] = useState([]);
   const getQuestions = async () => {
     //imports all questions from the database
-
+    await getDocs(collection(db, "question"))
+    .then((QuerySnapshot)=>{
+      const newData = querySnapshot.getDocs
+      .map((doc) => ({...doc.data(), id:doc.id }));
+      setQuestions(newData);
+      console.log(questions, newData);
+    })
     //assigns them to an array to be received  and then assigned to a set number of quizzes 
 
   }
 
+  useEffect(()=>{
+    getQuestions();
+  }, [])
+
   //function that receives the answer key from the database
+  //const { question, answers, answerKey, questionType } = questions[currentQuestion];
+  const [answerKey, setAnswerKey] = useState([]);
   const getAnswers = async () => {
     //imports answers from the database
-
+    await getDocs(collection(db, "answerKey"))
+    .then((QuerySnapshot)=>{
+      const newData = querySnapshot.getDocs
+      .map((doc) => ({...doc.data(), id:doc.id }));
+      setAnswerKey(newData);
+      console.log(answerKey, newData);
+    })
     //maps each question with it's respective answers to generate the multiple choice answers to 
 
+    useEffect(()=>{
+      setAnswerKey();
+    }, [])
   }
 
   //function that lets user move on to the next question
-  const onClickNextButton = () => {
+  const nextButton = () => {
     //move to the next question
-
+    setCurrentQuestion((prev) => prev + 1)
     //record answer in records
 
     //show a new question and answers
@@ -54,7 +97,11 @@ const LanguageQuiz = () => {
   //function that validates if the user's input is correct
   const checkAnswer = () => {
     //compare user picked answer and actual answer
-
+    if(answer === answerKey){
+      setSelectedAnswer(true)
+    } else {
+      setSelectedAnswer(false)
+    }
     //if answer is correct, count question correct, else count it incorrect
 
   }
@@ -87,6 +134,9 @@ const LanguageQuiz = () => {
   //function to store the results of this quiz in the search database
   const getQuizResults = () => {
     //get final score, correct questions and incorrect questions along with their answers
+    if(currentQuestion + 1 === 2){
+      
+    }
     //import it to the database under the 
   }
 
@@ -116,29 +166,46 @@ const LanguageQuiz = () => {
     //There are placeholders currently set for the question, the answer choices and the answer  along with the max number of questions to be received from the database  
     //There is also the Quiz results shown after the quiz is taken to show the resulting score an which questions are correct and incorrect
     <div className='quiz'>
-      {!showFinalResult ? (
+    {!showFinalResult ? (
+      <div>
+        <h1>Language Quiz</h1>
         <div>
-          <h1>Language Quiz</h1>
-          <div>
-            <h2> '(Question Placeholder)' </h2>
-            <ul>
-              <button>a1</button>
-              <button>a2</button>
-              <button>a3</button>
-              <button>a4</button>
-            </ul>
-            <button> Next </button>
-          </div>
-          <div className='index'> 1 of n</div>
+          <h2> {question} </h2>
+          {questionType === 'MCQ' ? (
+          <ul>
+            {answers.map((item, index) => (
+              <button
+                key={index}
+                onClick={() => {
+                  color === "grey"
+                  setSelectedAnswer(item)
+                  setSelectedAnswerIndex(index)
+                }}
+              >
+                {item}
+              </button>
+            ))}
+          </ul>
+          ) : (
+            <input
+              type="text"
+              value={selectedAnswer}
+              onChange={(e) => setSelectedAnswer(e.target.value)}
+              placeholder="Type your answer"
+            />
+          )}
+          <button onClick={nextButton}> Next </button>
         </div>
-      ) : (
-        <div className='results'>
-          <h2>Quiz Results</h2>
-          <p> Your score: n points</p>
-        </div>
-      )}
-    </div>
-  )
+        <div className='index'> {currentQuestion + 1} of 2</div>
+      </div>
+    ) : (
+      <div className='results'>
+        <h2>Quiz Results</h2>
+        <p> Your score: {finalScore} points</p>
+      </div>
+    )}
+  </div>
+)
 };
 
 export default LanguageQuiz;
