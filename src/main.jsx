8318e28a21449f964065a41e911ae63cf08bc857
amajalca <196114220+amajalca@users.noreply.github.com>
2025-04-30 10:51:29 -0500

    Fix homepage layout and enable scrolling

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { AuthProvider } from "./context/AuthContext";
+
 
 console.log("Starting React Application...");
 
@@ -14,7 +16,9 @@ if (!root) {
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
